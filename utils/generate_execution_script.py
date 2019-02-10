8aa263fc7185fba325c1f371ccaa2a64b921e891
Rungetf <runget@cs.uni-freiburg.de>
2019-02-10 11:34:28 +0100

    fix(utils/generate_execution_script): fix bug with resolve_path

--- utils/generate_execution_script.py
+++ utils/generate_execution_script.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from utils.configs_to_validation import _validate_config, _fill_config, _write_config, modes, timeouts
 
 
-def generate_execution_script(config_id, config, job_id, output_dir, mode, restore_path=None):
+def generate_execution_script(config_id, config, job_id, output_dir, mode, restore_path):
     if not _validate_config(config):
         config = _fill_config(config, mode)
 
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument("--job_id", default='12345', help="Id of the job that produced the config")
     parser.add_argument("--mode", default='learna', help="one of learna, meta_learna, meta_learna_adapt")
     parser.add_argument("--config_id", default='(0, 0, 0)', help="Id of the config received from BOHB")
-    parser.add_argument("--restore_path", default=None, type=Path, help="The path to load model from")
+    parser.add_argument("--restore_path", default="models/", type=Path, help="The path to load model from")
     parser.add_argument("--config", type=str, help="The config")
 
     args = parser.parse_args()
