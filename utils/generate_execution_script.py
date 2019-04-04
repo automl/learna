61eac8e5b6178e81be246612fd1c82efb3153ce2
Rungetf <runget@cs.uni-freiburg.de>
2019-04-04 10:38:36 +0200

    refac(utils): format generate_execution_script

--- utils/generate_execution_script.py
+++ utils/generate_execution_script.py
@@ -1,51 +1,79 @@
 from pathlib import Path
-from utils.configs_to_validation import _validate_config, _fill_config, _write_config, modes, timeouts
+from utils.configs_to_validation import (
+    _validate_config,
+    _fill_config,
+    _write_config,
+    modes,
+    timeouts,
+)
 
 
 def generate_execution_script(config_id, config, job_id, output_dir, mode, restore_path):
     if not _validate_config(config):
         config = _fill_config(config, mode)
 
-    with open(output_dir.joinpath('execution_scripts/' + job_id + '_' + config_id + '.sh'), 'w') as execution_script:
-        execution_script.write('#!/bin/bash\n')
-        execution_script.write('TARGET_STRUCTURE_PATH=$1\n')
-        execution_script.write('\n')
-        execution_script.write('source thirdparty/miniconda/miniconda/bin/activate learna\n')
-        execution_script.write('/usr/bin/time -f"%U" python -m src.learna.design_rna \\\n')
-        execution_script.write('  --mutation_threshold 5 \\\n')
+    with open(
+        output_dir.joinpath("execution_scripts/" + job_id + "_" + config_id + ".sh"), "w"
+    ) as execution_script:
+        execution_script.write("#!/bin/bash\n")
+        execution_script.write("TARGET_STRUCTURE_PATH=$1\n")
+        execution_script.write("\n")
+        execution_script.write(
+            "source thirdparty/miniconda/miniconda/bin/activate learna\n"
+        )
+        execution_script.write(
+            '/usr/bin/time -f"%U" python -m src.learna.design_rna \\\n'
+        )
+        execution_script.write("  --mutation_threshold 5 \\\n")
 
         _write_config(config, execution_script)
 
-        execution_script.write('  --target_structure_path $TARGET_STRUCTURE_PATH')
-        if mode == 'meta_learna' or mode == 'meta_learna_adapt':
-            execution_script.write('\\\n')
+        execution_script.write("  --target_structure_path $TARGET_STRUCTURE_PATH")
+        if mode == "meta_learna" or mode == "meta_learna_adapt":
+            execution_script.write("\\\n")
             execution_script.write(f"  --restore_path {restore_path.resolve()} ")
-            if mode == 'meta_learna':
-                execution_script.write('\\\n')
-                execution_script.write('  --stop_learning')
+            if mode == "meta_learna":
+                execution_script.write("\\\n")
+                execution_script.write("  --stop_learning")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
     import ast
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--output_dir", type=Path, help="Directory were the files will be stored"
     )
-    parser.add_argument("--job_id", default='12345', help="Id of the job that produced the config")
-    parser.add_argument("--mode", default='learna', help="one of learna, meta_learna, meta_learna_adapt")
-    parser.add_argument("--config_id", default='(0, 0, 0)', help="Id of the config received from BOHB")
-    parser.add_argument("--restore_path", default="models/", type=Path, help="The path to load model from")
+    parser.add_argument(
+        "--job_id", default="12345", help="Id of the job that produced the config"
+    )
+    parser.add_argument(
+        "--mode", default="learna", help="one of learna, meta_learna, meta_learna_adapt"
+    )
+    parser.add_argument(
+        "--config_id", default="(0, 0, 0)", help="Id of the config received from BOHB"
+    )
+    parser.add_argument(
+        "--restore_path", default="models/", type=Path, help="The path to load model from"
+    )
     parser.add_argument("--config", type=str, help="The config")
 
     args = parser.parse_args()
 
     config_id = args.config_id
-    config_id = config_id.replace('(', '')
-    config_id = config_id.replace(')', '')
-    config_id = config_id.replace(',', '_')
-    config_id = ''.join(config_id.split())
+    config_id = config_id.replace("(", "")
+    config_id = config_id.replace(")", "")
+    config_id = config_id.replace(",", "_")
+    config_id = "".join(config_id.split())
 
     config = ast.literal_eval(args.config)
 
-    generate_execution_script(config_id, config, args.job_id, args.output_dir.resolve(), args.mode, args.restore_path)
+    generate_execution_script(
+        config_id,
+        config,
+        args.job_id,
+        args.output_dir.resolve(),
+        args.mode,
+        args.restore_path,
+    )
