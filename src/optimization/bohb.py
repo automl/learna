380e5e68531341adbbad59ea2989dafd42cf09ae
Rungetf <runget@cs.uni-freiburg.de>
2019-04-13 12:40:20 +0200

    refac(optimization): learn from all training data

--- src/optimization/bohb.py
+++ src/optimization/bohb.py
@@ -14,12 +14,12 @@ import hpbandster.core.nameserver as hpns
 import hpbandster.core.result as hpres
 
 from hpbandster.optimizers import BOHB as BOHB
-from src.optimization.bohb_worker.LEARNA import DesignRNAWorker
-from src.optimization.bohb_worker.MetaLEARNA import L2DesignRNAWorker
+from src.optimization.learna_worker import LearnaWorker
+from src.optimization.meta_learna_worker import MetaLearnaWorker
 
 
 parser = argparse.ArgumentParser(
-    description="Example 1 - sequential and local execution."
+    description="Optimization pipeline for Learning to Design RNA"
 )
 parser.add_argument(
     "--min_budget",
@@ -51,7 +51,7 @@ parser.add_argument(
     "--run_id",
     type=str,
     help="A unique run id for this optimization run."
-         "An easy option is to use the job id of the clusters scheduler.",
+    "An easy option is to use the job id of the clusters scheduler.",
 )
 parser.add_argument("--data_dir", type=str, help="path where the datasets are stored")
 parser.add_argument(
@@ -63,9 +63,7 @@ parser.add_argument(
     help="A directory that is accessible for all processes, e.g. a NFS share.",
 )
 
-parser.add_argument(
-    "--mode", choices=["DesignRNA-rfam", "L2DesignRNA"], default="DesignRNA-rfam"
-)
+parser.add_argument("--mode", choices=["learna", "meta_learna"], default="learna")
 
 
 # args=parser.parse_args("--run_id test --nic_name lo --shared_directory /tmp --n_cores 4 --data_dir src/data --mode L2DesignRNA".split())
@@ -73,18 +71,18 @@ args = parser.parse_args()
 
 os.makedirs(args.shared_directory, exist_ok=True)
 
-if args.mode == "DesignRNA-rfam":
-    worker_cls = DesignRNAWorker
+if args.mode == "learna":
+    worker_cls = LearnaWorker
     worker_args = dict(
         data_dir=args.data_dir, num_cores=args.n_cores, train_sequences=range(1, 100, 3)
     )
 
-if args.mode == "L2DesignRNA":
-    worker_cls = L2DesignRNAWorker
+if args.mode == "meta_learna":
+    worker_cls = MetaLearnaWorker
     worker_args = dict(
         data_dir=args.data_dir,
         num_cores=args.n_cores,
-        train_sequences=range(1, 8096),
+        train_sequences=range(1, 65000),
         validation_timeout=60,
     )
 
@@ -136,7 +134,7 @@ bohb = BOHB(
     ping_interval=600,
     working_directory=args.shared_directory,
 )
-res = bohb.run(n_iterations=args.n_iterations)
+res = bohb.run(n_iterations=args.n_iterations, min_n_workers=20)
 
 
 # In a cluster environment, you usually want to store the results for later analysis.
