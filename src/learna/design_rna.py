c2b60d663e9eaf69ed41ee0ffea1c313b1058f53
Rungetf <runget@cs.uni-freiburg.de>
2019-04-13 12:29:38 +0200

    refac(src/learna): refac tiny parts of design_rna

--- src/learna/design_rna.py
+++ src/learna/design_rna.py
@@ -25,7 +25,7 @@ def _get_episode_finished(timeout, stop_once_solved):
 
         candidate_solution = env.design.primary
         last_reward = runner.episode_rewards[-1]
-        last_fractional_hamming = env.episodes_info[-1].fractional_hamming_distance
+        last_fractional_hamming = env.episodes_info[-1].normalized_hamming_distance
         elapsed_time = time.time() - start_time
         print(elapsed_time, last_reward, last_fractional_hamming, candidate_solution)
 
@@ -38,34 +38,28 @@ def _get_episode_finished(timeout, stop_once_solved):
 
 
 def design_rna(
-    dataset,
-    data_dir,
-    target_structure_ids,
-    target_structure_path,
+    dot_brackets,
     timeout,
     restore_path,
     stop_learning,
     restart_timeout,
     network_config,
     agent_config,
-    environment_config,
+    env_config,
 ):
     """
     Main function for RNA design. Instantiate an environment and an agent to run in a
     tensorforce runner.
 
     Args:
-        dataset: The name of the benchmark the dataset comes from.
-        data_dir: The path to the data of target secondary structures.
-        target_structure_ids: If given, only solutions for certain target structure are designed.
-        target_structure_path: Path to a specific target structure to predict for.
+        TODO
         timeout: Maximum time to run.
         restore_path: Path to restore saved configurations/models from.
         stop_learning: If set, no weight updates are performed (Meta-LEARNA).
         restart_timeout: Time interval for restarting of the agent.
         network_config: The configuration of the network.
         agent_config: The configuration of the agent.
-        environment_config: The configuration of the environment.
+        env_config: The configuration of the environment.
 
     Returns:
         Episode information.
@@ -76,15 +70,11 @@ def design_rna(
         allow_soft_placement=True,
         device_count={"CPU": 1},
     )
-    environment_config.use_conv = any(map(lambda x: x > 1, network_config.conv_sizes))
-    environment_config.use_embedding = bool(network_config.embedding_size)
-    environment = RnaDesignEnvironment(
-        dataset,
-        data_dir,
-        target_structure_ids,
-        target_structure_path,
-        environment_config=environment_config,
-    )
+
+    env_config.use_conv = any(map(lambda x: x > 1, network_config.conv_sizes))
+    env_config.use_embedding = bool(network_config.embedding_size)
+    environment = RnaDesignEnvironment(dot_brackets, env_config)
+
     network = get_network(network_config)
     # Runner restarts the agent by calling get_agent again
     get_agent = get_agent_fn(
@@ -96,9 +86,7 @@ def design_rna(
     )
     runner = Runner(get_agent, environment)
 
-    # Perform multi sequence training if single id or path is given
-    single_id = target_structure_ids and len(target_structure_ids) == 1
-    stop_once_solved = single_id or target_structure_path
+    stop_once_solved = len(dot_brackets) == 1
     runner.run(
         deterministic=False,
         restart_timeout=restart_timeout,
@@ -111,6 +99,7 @@ def design_rna(
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
+    from ..data.parse_dot_brackets import parse_dot_brackets
 
     parser = argparse.ArgumentParser()
 
@@ -143,9 +132,6 @@ if __name__ == "__main__":
         "--mutation_threshold", type=int, help="Enable MUTATION with set threshold"
     )
     parser.add_argument(
-        "--include_mutation", action="store_true", help="Include MUTATION in rewards"
-    )
-    parser.add_argument(
         "--reward_exponent", default=1, type=float, help="Exponent for reward shaping"
     )
     parser.add_argument(
@@ -170,20 +156,10 @@ if __name__ == "__main__":
         "--entropy_regularization", type=float, default=1.5e-3, help="The output entropy"
     )
     parser.add_argument(
-        "--likelihood_ratio_clipping",
-        type=float,
-        default=0.2,
-        help="Likelihood ratio clipping for policy gradient",
-    )
-    parser.add_argument(
         "--restart_timeout", type=int, help="Time after which to restart the agent"
     )
-    parser.add_argument("--fc_activation", type=str, help="Activation function")
     parser.add_argument("--lstm_units", type=int, help="The number of lstm units")
     parser.add_argument("--num_lstm_layers", type=int, help="The number of lstm layers")
-    parser.add_argument(
-        "--optimization_steps", type=int, help="The number of optimization steps"
-    )
     parser.add_argument("--embedding_size", type=int, help="The size of the embedding")
 
     args = parser.parse_args()
@@ -193,7 +169,6 @@ if __name__ == "__main__":
         conv_channels=args.conv_channels,
         num_fc_layers=args.num_fc_layers,
         fc_units=args.fc_units,
-        fc_activation=args.fc_activation,
         lstm_units=args.lstm_units,
         num_lstm_layers=args.num_lstm_layers,
         embedding_size=args.embedding_size,
@@ -202,27 +177,27 @@ if __name__ == "__main__":
         learning_rate=args.learning_rate,
         batch_size=args.batch_size,
         entropy_regularization=args.entropy_regularization,
-        likelihood_ratio_clipping=args.likelihood_ratio_clipping,
         random_agent=args.random_agent,
-        optimization_steps=args.optimization_steps,
     )
-    environment_config = RnaDesignEnvironmentConfig(
+    env_config = RnaDesignEnvironmentConfig(
         mutation_threshold=args.mutation_threshold,
-        include_mutation=args.include_mutation,
         reward_exponent=args.reward_exponent,
         state_radius=args.state_radius,
     )
-
-    design_rna(
+    dot_brackets = parse_dot_brackets(
         dataset=args.dataset,
         data_dir=args.data_dir,
         target_structure_ids=args.target_structure_ids,
         target_structure_path=args.target_structure_path,
+    )
+
+    design_rna(
+        dot_brackets,
         timeout=args.timeout,
         restore_path=args.restore_path,
         stop_learning=args.stop_learning,
         restart_timeout=args.restart_timeout,
         network_config=network_config,
         agent_config=agent_config,
-        environment_config=environment_config,
+        env_config=env_config,
     )
