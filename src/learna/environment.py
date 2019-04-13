8eb2ef30f9abe001cf0b05f699b86c8b564dd6e5
Rungetf <runget@cs.uni-freiburg.de>
2019-04-13 12:32:31 +0200

    refac(src/learna): reimplement LIS, pairing_encoding; some updates, fixes

--- src/learna/environment.py
+++ src/learna/environment.py
@@ -1,6 +1,5 @@
 import time
 
-from pathlib import Path
 from itertools import product
 from dataclasses import dataclass
 from distance import hamming
@@ -19,7 +18,6 @@ class RnaDesignEnvironmentConfig:
     Default values describe:
         mutation_threshold: Defines the minimum distance needed before applying the local
             improvement step.
-        include_mutation: If not set, the local improvement step is not part of the reward
         reward_exponent: A parameter to shape the reward function.
         state_radius: The state representation is a (2*<state_radius> + 1)-gram
             at each position.
@@ -28,7 +26,6 @@ class RnaDesignEnvironmentConfig:
     """
 
     mutation_threshold: int = 5
-    include_mutation: bool = True
     reward_exponent: float = 1.0
     state_radius: int = 5
     use_conv: bool = True
@@ -49,96 +46,67 @@ def _string_difference_indices(s1, s2):
     return [index for index in range(len(s1)) if s1[index] != s2[index]]
 
 
-def _encode_dot_bracket(secondary, environment_config):
+def _encode_dot_bracket(secondary, env_config):
     """
     Encode the dot_bracket notated target structure. The encoding can either be binary
     or by the embedding layer.
 
     Args:
         secondary: The target structure in dot_bracket notation.
-        environment_config: The configuration of the environment.
+        env_config: The configuration of the environment.
 
     Returns:
         List of encoding for each site of the padded target structure.
     """
-    padding = "=" * environment_config.state_radius
+    padding = "=" * env_config.state_radius
     padded_secondary = padding + secondary + padding
 
-    if environment_config.use_embedding:
+    if env_config.use_embedding:
         site_encoding = {".": 0, "(": 1, ")": 2, "=": 3}
     else:
         site_encoding = {".": 0, "(": 1, ")": 1, "=": 0}
 
     # Sites corresponds to 1 pixel with 1 channel if convs are applied directly
-    if environment_config.use_conv and not environment_config.use_embedding:
+    if env_config.use_conv and not env_config.use_embedding:
         return [[site_encoding[site]] for site in padded_secondary]
     return [site_encoding[site] for site in padded_secondary]
 
 
-def _get_index_diff(string):
+def _encode_pairing(secondary):
+    """TODO
     """
-    Compute the index of a corresponding closing bracket to an opening bracket.
-
-    Args:
-        string: The string to compute the index of corresponding closing bracket for.
-
-    Returns:
-        The index of the corresponding closing bracket for the first opening bracket
-    """
-    bracket_count = 0
-    for index, base in enumerate(string):
-        if base == "(":
-            bracket_count += 1
-        elif base == ")":
-            bracket_count -= 1
-        else:
-            continue
-        if bracket_count == 0:
-            return index
-
-
-def _encode_gap(secondary):
-    """
-    Encode the target structure using gap encoding.
-
-    Args:
-        secondary: The secondary structure that needs to get gap encoded.
-
-    Returns:
-        Gap encoding of the target secondary structure.
-
-    Note:
-        There exists a O(n) implementation of this algorithm using a stack.
-    """
-    gap_encoding = [0 for x in secondary]
-    for index, base in enumerate(secondary):
-        if base != "(":
-            continue
-        else:
-            index_diff = _get_index_diff(secondary[index:])
-            gap_encoding[index] = index_diff
-            gap_encoding[index + index_diff] = index_diff * -1
-    return gap_encoding
+    pairing_encoding = [None] * len(secondary)
+    stack = []
+    for index, symbol in enumerate(secondary, 0):
+        if symbol == "(":
+            stack.append(index)
+        elif symbol == ")":
+            paired_site = stack.pop()
+            pairing_encoding[paired_site] = index
+            pairing_encoding[index] = paired_site
+    return pairing_encoding
 
 
 class _Target(object):
-    """
+    """TODO
     Class of the target structure. Provides encodings and id.
     """
 
-    def __init__(self, dot_bracket, environment_config, id=None):
+    _id_counter = 0
+
+    def __init__(self, dot_bracket, env_config):
         """
         Initialize a target structure.
 
         Args:
              dot_bracket: dot_bracket encoded target structure.
-             environment_config: The environment configuration.
-             id: The id of the target structure.
+             env_config: The environment configuration.
         """
-        self.id = id
+        _Target._id_counter += 1
+        self.id = _Target._id_counter  # For processing results
         self.dot_bracket = dot_bracket
-        self._gap_encoding = _encode_gap(self.dot_bracket)
-        self.padded_encoding = _encode_dot_bracket(self.dot_bracket, environment_config)
+        self._pairing_encoding = _encode_pairing(self.dot_bracket)
+        self.padded_encoding = _encode_dot_bracket(self.dot_bracket, env_config)
 
     def __len__(self):
         return len(self.dot_bracket)
@@ -151,11 +119,9 @@ class _Target(object):
             site: The site to check the pairing site for.
 
         Returns:
-            The site that pairs with <site> if exists.
+            The site that pairs with <site> if exists.TODO
         """
-        if self.dot_bracket[site] == ".":  # Has no paired site
-            return None
-        return self._gap_encoding[site] + site
+        return self._pairing_encoding[site]
 
 
 class _Design(object):
@@ -179,6 +145,7 @@ class _Design(object):
         else:
             self._primary_list = [None] * length
         self._dot_bracket = None
+        self._current_site = 0
 
     def get_mutated(self, mutations, sites):
         """
@@ -205,6 +172,7 @@ class _Design(object):
             site: The site to which the nucleotide is assigned to.
             paired_site: defines if the site is assigned with a base pair or not.
         """
+        self._current_site += 1
         if paired_site:
             base_current, base_paired = self.action_to_pair[action]
             self._primary_list[site] = base_current
@@ -213,14 +181,17 @@ class _Design(object):
             self._primary_list[site] = self.action_to_base[action]
 
     @property
-    def primary(self):
-        return "".join(self._primary_list)
+    def first_unassigned_site(self):
+        try:
+            while self._primary_list[self._current_site] is not None:
+                self._current_site += 1
+            return self._current_site
+        except IndexError:
+            return None
 
     @property
-    def dot_bracket(self):
-        if not self._dot_bracket:
-            self._dot_bracket, _ = fold(self.primary)
-        return self._dot_bracket
+    def primary(self):
+        return "".join(self._primary_list)
 
 
 def _random_epoch_gen(data):
@@ -235,49 +206,16 @@ def _random_epoch_gen(data):
             yield data[i]
 
 
-def _get_target_gen(dataset, data_dir, target_ids, target_path, environment_config):
-    """
-    Generate the targets for next epoch.
-
-    Args:
-        dataset: The name of the benchmark to use targets from.
-        data_dir: The directory of the target structures.
-        target_ids: Use specific targets by ids.
-        target path: Specify a path to the targets.
-        environment_config: The configuration of the environment.
-
-    Returns:
-        An epoch generator for the specified target structures.
-    """
-
-    def _get_data_id_from_path(path):
-        return int(path.name[:-4])
-
-    if target_path:
-        target_paths = [target_path]
-    elif target_ids:
-        target_paths = [Path(data_dir, dataset, f"{id_}.rna") for id_ in target_ids]
-    else:
-        target_paths = list(Path(data_dir, dataset).glob("*.rna"))
-    target_ids = [_get_data_id_from_path(path) for path in target_paths]
-    dot_brackets = [data_path.read_text().rstrip() for data_path in target_paths]
-    targets = [
-        _Target(dot_bracket, environment_config, id_)
-        for dot_bracket, id_ in zip(dot_brackets, target_ids)
-    ]
-    return _random_epoch_gen(targets)
-
-
 @dataclass
 class EpisodeInfo:
     """
     Information class.
     """
 
-    __slots__ = ["target_id", "time", "fractional_hamming_distance"]
+    __slots__ = ["target_id", "time", "normalized_hamming_distance"]
     target_id: int
     time: float
-    fractional_hamming_distance: float
+    normalized_hamming_distance: float
 
 
 class RnaDesignEnvironment(Environment):
@@ -285,41 +223,20 @@ class RnaDesignEnvironment(Environment):
     The environment for RNA design using deep reinforcement learning.
     """
 
-    def __init__(
-        self,
-        dataset,
-        data_dir,
-        target_structure_ids,
-        target_structure_path,
-        environment_config,
-    ):
-        """
+    def __init__(self, dot_brackets, env_config):
+        """TODO
         Initialize an environemnt.
 
         Args:
-            dataset: The benchmark to use target structures from.
-            data_dir: The path to the directory with all target structures.
-            target_structure_ids: The ids of the target structures to use (optional).
-            target_structure_path: The path to a target structure to use (optional).
-            environment_config: The configuration of the environment.
+            env_config: The configuration of the environment.
         """
-        self._mutation_threshold = environment_config.mutation_threshold
-        self._include_mutation = environment_config.include_mutation
-        self._reward_exponent = environment_config.reward_exponent
-        self._state_radius = environment_config.state_radius
-        self._use_embedding = environment_config.use_embedding
-        self._use_conv = environment_config.use_conv
+        self._env_config = env_config
 
-        self._target_gen = _get_target_gen(
-            dataset,
-            data_dir,
-            target_structure_ids,
-            target_structure_path,
-            environment_config,
-        )
+        targets = [_Target(dot_bracket, self._env_config) for dot_bracket in dot_brackets]
+        self._target_gen = _random_epoch_gen(targets)
+
         self.target = None
         self.design = None
-        self._step = None
         self.episodes_info = []
 
     def __str__(self):
@@ -337,27 +254,7 @@ class RnaDesignEnvironment(Environment):
         """
         self.target = next(self._target_gen)
         self.design = _Design(len(self.target))
-        self._step = 0
         return self._get_state()
-
-    def _local_improvement(self):
-        """
-        Compute Hamming distance of locally improved candidate solutions.
-
-        Returns:
-            The minimum Hamming distance of all imporved candidate solutions.
-        """
-        differing_sites = _string_difference_indices(
-            self.target.dot_bracket, self.design.dot_bracket
-        )
-        hamming_distances = []
-        for mutation in product("AGCU", repeat=len(differing_sites)):
-            mutated = self.design.get_mutated(mutation, differing_sites)
-            hamming_distance = hamming(mutated.dot_bracket, self.target.dot_bracket)
-            hamming_distances.append(hamming_distance)
-            if hamming_distance == 0:  # For better timing results
-                return 0
-        return min(hamming_distances)
 
     def _apply_action(self, action):
         """
@@ -366,11 +263,9 @@ class RnaDesignEnvironment(Environment):
         Args:
             action: The action chosen by the agent.
         """
-        paired_site = self.target.get_paired_site(self._step)
-        if paired_site:
-            self.design.assign_sites(action, self._step, paired_site)
-        else:
-            self.design.assign_sites(action, self._step)
+        current_site = self.design.first_unassigned_site
+        paired_site = self.target.get_paired_site(current_site)  # None for unpaired sites
+        self.design.assign_sites(action, current_site, paired_site)
 
     def _get_state(self):
         """
@@ -379,11 +274,32 @@ class RnaDesignEnvironment(Environment):
         Returns:
             The next state.
         """
+        start = self.design.first_unassigned_site
         return self.target.padded_encoding[
-            self._step : self._step + 2 * self._state_radius + 1
+            start : start + 2 * self._env_config.state_radius + 1
         ]
 
-    def _get_next_reward(self, terminal):
+    def _local_improvement(self, folded_design):
+        """
+        Compute Hamming distance of locally improved candidate solutions.
+
+        Returns:
+            The minimum Hamming distance of all imporved candidate solutions.
+        """
+        differing_sites = _string_difference_indices(
+            self.target.dot_bracket, folded_design
+        )
+        hamming_distances = []
+        for mutation in product("AGCU", repeat=len(differing_sites)):
+            mutated = self.design.get_mutated(mutation, differing_sites)
+            folded_mutated, _ = fold(mutated.primary)
+            hamming_distance = hamming(folded_mutated, self.target.dot_bracket)
+            hamming_distances.append(hamming_distance)
+            if hamming_distance == 0:  # For better timing results
+                return 0
+        return min(hamming_distances)
+
+    def _get_reward(self, terminal):
         """
         Compute the reward after assignment of all nucleotides.
 
@@ -396,27 +312,22 @@ class RnaDesignEnvironment(Environment):
         if not terminal:
             return 0
 
-        hamming_distance = hamming(self.design.dot_bracket, self.target.dot_bracket)
-        apply_mutation = (
-            self._mutation_threshold and 0 < hamming_distance < self._mutation_threshold
-        )
-        if apply_mutation:
-            locally_improved_distance = self._local_improvement()
-            # If mutation found a solution use result regardless of self._include_mutation
-            if self._include_mutation or locally_improved_distance == 0:
-                hamming_distance = locally_improved_distance
+        folded_design, _ = fold(self.design.primary)
+        hamming_distance = hamming(folded_design, self.target.dot_bracket)
+        if 0 < hamming_distance < self._env_config.mutation_threshold:
+            hamming_distance = self._local_improvement(folded_design)
 
-        fractional_hamming_distance = hamming_distance / len(self.target)
+        normalized_hamming_distance = hamming_distance / len(self.target)
 
         # For hparam optimization
         episode_info = EpisodeInfo(
             target_id=self.target.id,
             time=time.time(),
-            fractional_hamming_distance=fractional_hamming_distance,
+            normalized_hamming_distance=normalized_hamming_distance,
         )
         self.episodes_info.append(episode_info)
 
-        return (1 - fractional_hamming_distance) ** self._reward_exponent
+        return (1 - normalized_hamming_distance) ** self._env_config.reward_exponent
 
     def execute(self, actions):
         """
@@ -431,11 +342,10 @@ class RnaDesignEnvironment(Environment):
             reward: The reward if at terminal timestep, else 0.
         """
         self._apply_action(actions)
-        self._step += 1
 
-        terminal = self._step == len(self.target)
+        terminal = self.design.first_unassigned_site is None
         state = None if terminal else self._get_state()
-        reward = self._get_next_reward(terminal)
+        reward = self._get_reward(terminal)
 
         return state, terminal, reward
 
@@ -444,10 +354,10 @@ class RnaDesignEnvironment(Environment):
 
     @property
     def states(self):
-        type = "int" if self._use_embedding else "float"
-        if self._use_conv and not self._use_embedding:
-            return dict(type=type, shape=(1 + 2 * self._state_radius, 1))
-        return dict(type=type, shape=(1 + 2 * self._state_radius,))
+        type = "int" if self._env_config.use_embedding else "float"
+        if self._env_config.use_conv and not self._env_config.use_embedding:
+            return dict(type=type, shape=(1 + 2 * self._env_config.state_radius, 1))
+        return dict(type=type, shape=(1 + 2 * self._env_config.state_radius,))
 
     @property
     def actions(self):
